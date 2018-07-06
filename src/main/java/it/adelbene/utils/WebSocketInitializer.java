d3d7f96549b7c992c8db948f6bdb5944dd8ceb11
Andrea Del Bene <an.delbene@gmail.com>
2018-07-06 22:08:07 +0200

    Added web socket idle time config

---
+++ src/main/java/it/adelbene/utils/WebSocketInitializer.java
@@ -0,0 +1,21 @@
+package it.adelbene.utils;
+
+import java.util.Set;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.websocket.server.ServerContainer;
+
+public class WebSocketInitializer implements ServletContainerInitializer
+{
+
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException
+	{
+		ServerContainer wsContainer = (ServerContainer)ctx
+			.getAttribute("javax.websocket.server.ServerContainer");
+		wsContainer.setDefaultMaxSessionIdleTimeout(3600000);
+	}
+
+}
